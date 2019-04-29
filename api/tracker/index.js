a19362ba69edb82a71c0f4199709bcda9febd5e5
ethdevss <ethdevss@gmail.com>
2019-04-29 16:25:09 +0900

    add JWT Authenticate Logic for security

--- api/tracker/index.js
+++ api/tracker/index.js
@@ -2,19 +2,70 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./tracker.ctrl');
 
-router.get('/event/:contractAddress/deposit', ctrl.getDepositEvents);
-router.get('/event/:contractAddress/withdraw', ctrl.getWithdrawEvents);
-router.get('/event/:contractAddress/trade/:fromBlock/:toBlock', ctrl.getTraderList);
-router.get('/event/:contractAddress/users', ctrl.getActiveUsers);
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
 
-router.get('/event/:contractAddress', ctrl.startTransferTracking);
+const API_HEADERS = {
+  JWT_TOKEN: 'jwt-token',
+  JWT_CONSUMER: 'jwt-consumer'
+};
 
-router.get('/block/:fromBlock/:toBlock', ctrl.getBlockInfo);
+const MESSAGES = {
+  NOT_FOUND: 'Valid headers not present',
+  TOKEN_EXPIRES: 'Downloasd token expired',
+  NOT_VALID_CLIENT: 'Not a valid client',
+};
 
-router.get('/subscribe/:contractAddress/transactions/pending', ctrl.subscribePending);
-router.get('/unsubscribe/:contractAddress/transactions/pending', ctrl.unsubscribePending);
+const SECRETS = {
+  'de-faker-web': 'secret',
+}
 
-router.get('/subscribe/event/:contractAddress/deposit', ctrl.depositTracking);
-router.get('/subscribe/event/:contractAddress/withdraw', ctrl.withdrawTracking);
+const JWTVerifier = async (req, res, next) => {
+  const jwtToken = req.headers[API_HEADERS.JWT_TOKEN];
+  const jwtConsumer = req.headers[API_HEADERS.JWT_CONSUMER];
+  const payload = {};
+
+  console.log('jwtToken: ' + jwtToken);
+  console.log('jwtConsumer: ' + jwtConsumer);
+
+  if(!jwtToken || !jwtConsumer){
+    return res.status(400).json({message: MESSAGES.NOT_FOUND});
+  }
+
+  try {
+    const secret = SECRETS[jwtConsumer];
+    if(!secret){
+      return res.status(403).json({
+        message: MESSAGES.NOT_VALID_CLIENT
+      });
+    }
+    try{
+      jwt.verify(jwtToken, secret); // verify only token not data.
+      return next();
+    } catch(err){
+      console.log(err);
+      return res.status(403).json({
+        messages: MESSAGES.NOT_FOUND
+      });
+    }
+  } catch(error){
+    return next(error);
+  }
+}
+
+router.get('/event/:contractAddress/deposit', JWTVerifier, ctrl.getDepositEvents);
+router.get('/event/:contractAddress/withdraw', JWTVerifier, ctrl.getWithdrawEvents);
+router.get('/event/:contractAddress/trade/:fromBlock/:toBlock', JWTVerifier, ctrl.getTraderList);
+router.get('/event/:contractAddress/users', JWTVerifier, ctrl.getActiveUsers);
+
+router.get('/event/:contractAddress', JWTVerifier, ctrl.startTransferTracking);
+
+router.get('/block/:fromBlock/:toBlock', JWTVerifier, ctrl.getBlockInfo);
+
+router.get('/subscribe/:contractAddress/transactions/pending', JWTVerifier, ctrl.subscribePending);
+router.get('/unsubscribe/:contractAddress/transactions/pending', JWTVerifier, ctrl.unsubscribePending);
+
+router.get('/subscribe/event/:contractAddress/deposit', JWTVerifier, ctrl.depositTracking);
+router.get('/subscribe/event/:contractAddress/withdraw', JWTVerifier, ctrl.withdrawTracking);
 
 module.exports = router;
